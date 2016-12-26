8aa31df5c312962f79dd6dd9125b75a45782dc43
Aaron St. John <aaronbstjohn@gmail.com>
2016-12-25 22:01:22 -0500

    Added some haxe libraries

--- vagrant-scripts/bootstrap_haxe_user.sh
+++ vagrant-scripts/bootstrap_haxe_user.sh
@@ -8,3 +8,4 @@ sudo yes | haxelib git tink_core https://github.com/haxetink/tink_core.git
 sudo yes | haxelib git minject https://github.com/massiveinteractive/minject.git
 sudo yes | haxelib git ash https://github.com/nadako/Ash-Haxe.git
 sudo yes | haxelib git hxmath https://github.com/tbrosman/hxmath.git 
+sudo yes | haxelib git openfl-gui https://github.com/tienery/openfl-gui.git 
